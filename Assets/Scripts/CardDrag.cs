05711c5fab012ffd1cdc16df208283d9705171c6
Pepe Coral <coritochico@gmail.com>
2025-10-28 16:16:04 +0100

    feat: added basic mouse interaction with a card and triggers decisions

---
+++ Assets/Scripts/CardDrag.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Collider2D))]
+public class CardDragNewInput : MonoBehaviour
+{
+    private Camera mainCamera;
+    private Vector3 offset;
+    private bool isDragging = false;
+    [SerializeField] private Vector3 targetPosition;
+    [SerializeField] private float returnSpeed;
+    [SerializeField] private float followSpeed;
+
+    [SerializeField] private float activationDistanceThreshold;
+    [SerializeField] private float targetXOnActivation;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        targetPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        var mouse = Mouse.current;
+        if (mouse == null) return;
+        Vector3 mouseWorldPos = getMouseWorldPosition(mouse);
+
+        if (mouse.leftButton.wasPressedThisFrame)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector2.zero);
+            if (hit.collider != null && hit.collider.gameObject == gameObject)
+            {
+                startDragging(mouseWorldPos);
+            }
+        }
+
+        if (mouse.leftButton.isPressed && isDragging)
+        {
+            transform.position = Vector3.Lerp(transform.position, mouseWorldPos + offset,Time.deltaTime * followSpeed);
+        }
+
+        if (mouse.leftButton.wasReleasedThisFrame && isDragging)
+        {
+            isDragging = false;
+            setNewTargetPosition();
+        }
+
+        if (!isDragging)
+        {
+            returnToTargetPosition();
+        }
+
+
+        if(Mathf.Abs(Mathf.Abs(transform.position.x) - targetXOnActivation) < 0.01f)
+        {
+            if(transform.position.x < 0) 
+            { GameManager.Instance.MakeDecision(true); }
+            else 
+            { GameManager.Instance.MakeDecision(false); }
+
+                returnToTheCenter();
+        } 
+    }
+
+    private void returnToTheCenter()
+    {
+        targetPosition = Vector3.zero;
+        transform.position = Vector3.zero;
+    }
+
+    private void startDragging(Vector3 mouseWorldPos)
+    {
+        isDragging = true;
+        offset = transform.position - mouseWorldPos;
+    }
+
+    private void setNewTargetPosition()
+    {
+        float targetX = transform.position.x > activationDistanceThreshold ? targetXOnActivation : (transform.position.x < -activationDistanceThreshold ? -targetXOnActivation : 0);
+        targetPosition = new Vector3(targetX, 0);
+    }
+
+    private void returnToTargetPosition()
+    {
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * returnSpeed);
+    }
+
+    private Vector3 getMouseWorldPosition(Mouse mouse)
+    {
+        return mainCamera.ScreenToWorldPoint(new Vector3(
+            mouse.position.x.ReadValue(),
+            mouse.position.y.ReadValue(),
+           0f
+        ));
+    }
+}
